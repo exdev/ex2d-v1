e273c7878c9363779263cbe49bae3149b205b876
jwu <KarasAya@gmail.com>
2012-02-26 19:48:59 +0800

    Fix pixel perfect problem

--- ex2d_dev/Assets/ex2D/Core/Component/Camera/exPixelPerfectCamera.cs
+++ ex2d_dev/Assets/ex2D/Core/Component/Camera/exPixelPerfectCamera.cs
@@ -62,7 +62,7 @@ public class exPixelPerfectCamera : MonoBehaviour {
         exPixelPerfect[] ppfs = GameObject.FindObjectsOfType ( typeof(exPixelPerfect) ) as exPixelPerfect[];
         foreach ( exPixelPerfect ppf in ppfs ) {
             exSpriteBase sprite = ppf.GetComponent<exSpriteBase>();
-            if ( sprite == null || sprite.renderCamera != this )
+            if ( sprite == null || sprite.renderCamera != camera )
                 continue;
 
             Vector3 toCamera = sprite.transform.position - transform.position;
@@ -120,9 +120,10 @@ public class exPixelPerfectCamera : MonoBehaviour {
             exPixelPerfect[] ppfs = GameObject.FindObjectsOfType ( typeof(exPixelPerfect) ) as exPixelPerfect[];
             foreach ( exPixelPerfect ppf in ppfs ) {
                 exSpriteBase sprite = ppf.GetComponent<exSpriteBase>();
-                if ( sprite == null || sprite.renderCamera != this )
+                if ( sprite == null || sprite.renderCamera != camera )
                     continue;
 
+                Debug.Log("sprite name = " + sprite.name);
                 Vector3 toCamera = sprite.transform.position - transform.position;
                 CalculatePixelPerfectScale ( sprite, toCamera.magnitude );
                 sprite.Commit();
