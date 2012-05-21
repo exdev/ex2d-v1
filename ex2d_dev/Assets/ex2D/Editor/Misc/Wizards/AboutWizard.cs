761ebb92bafebdf24286e72e9c351a120357f73e
jwu <KarasAya@gmail.com>
2012-05-21 13:57:47 +0800

    update version number

--- ex2d_dev/Assets/ex2D/Editor/Misc/Wizards/AboutWizard.cs
+++ ex2d_dev/Assets/ex2D/Editor/Misc/Wizards/AboutWizard.cs
@@ -45,7 +45,7 @@ class AboutWizard : ScriptableWizard {
         GUILayout.Space (10);
         GUILayout.BeginHorizontal();
             GUILayout.Space (10);
-            GUILayout.Label("ex2D v1.2.5 (120510)");
+            GUILayout.Label("ex2D v1.2.6 (120521)");
         GUILayout.EndHorizontal();
 
         string support = "";
